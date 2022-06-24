8738795cb203dc816f4dc82c1134644f2181f958
yuyatinnefeld <yuyawamo@gmail.com>
2022-06-24 11:17:08 +0200

    feat: read yaml file

---
+++ step5/README.md
@@ -0,0 +1,13 @@
+```bash
+# create initial fiels
+
+# build image
+docker build -t my-golang-image .
+
+# run container
+docker run -it --rm --name my-golang-app my-golang-image
+
+# run go in the container
+ls
+go run .
+```
---
+++ step5/mymodule/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+     "fmt"
+     "io/ioutil"
+     "log"
+     "gopkg.in/yaml.v3"
+)
+
+func main() {
+
+     yfile, err := ioutil.ReadFile("conf.yaml")
+
+     if err != nil {
+          log.Fatal(err)
+     }
+
+     data := make(map[interface{}]interface{})
+
+     err2 := yaml.Unmarshal(yfile, &data)
+
+     if err2 != nil {
+          log.Fatal(err2)
+     }
+
+     for k, v := range data {
+          fmt.Printf("%s -> %d\n", k, v)
+     }
+}
