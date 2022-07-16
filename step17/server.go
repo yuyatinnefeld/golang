898bb1f62e2536d5e6576624621fd3c345be442f
yuyatinnefeld <yuyawamo@gmail.com>
2022-07-16 18:44:57 +0200

    feat: update mongodb setup

--- step17/server.go
+++ step17/server.go
@@ -4,24 +4,17 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 
-	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found")
-	}
+	var uri string = "mongodb://admin:password@localhost:27017/?maxPoolSize=20&w=majority"
 
-	uri = "mongodb://admin:password@localhost:27017/?maxPoolSize=20&w=majority"
-	if uri == "" {
-		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
-	}
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+
 	if err != nil {
 		panic(err)
 	}
@@ -32,10 +25,12 @@ func main() {
 		}
 	}()
 
-	coll := client.Database("sample_mflix").Collection("movies")
-	title := "Back to the Future"
+	coll := client.Database("db").Collection("jobs")
+
+	title := "DevOps Engineer"
 
 	var result bson.M
+
 	err = coll.FindOne(context.TODO(), bson.D{{"title", title}}).Decode(&result)
 	if err == mongo.ErrNoDocuments {
 		fmt.Printf("No document was found with the title %s\n", title)
